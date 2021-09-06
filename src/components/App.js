19bd92c6bb797a6989ae22165f9c2e7f502523a1
ask03 <aykim043@gmail.com>
2021-09-06 17:34:16 +0900

    refactored mint page

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
 import Web3 from 'web3';
 import PlayBirdMansion from '../abis/PlayBirdMansion.json'
 
 class App extends Component {
 
     async componentDidMount() {
-      await this.loadBlockChainData(this.props.dispatch)
+      await this.loadBlockChainData()
     }
 
-    async loadBlockChainData(dispatch) {
+    async loadBlockChainData() {
       if(typeof window.ethereum !== 'undefined') {
         const web3 = new Web3(window.ethereum)
         const netId = await web3.eth.net.getId()
@@ -37,46 +36,87 @@ class App extends Component {
 
     }
 
-    async mintBird(e) {
-      e.preventDefault()
+    async mintBirds(numberOfBirds, amount) {
       if(this.state.token !== 'undefined') {
         try {
-          await this.state.token.methods.mintBird(1).send({ value: (10**16)*2, from: this.state.account })
+          await this.state.token.methods.mintBird(numberOfBirds).send({ value: amount, from: this.state.account })
         } catch (e) {
           console.log('Error, mintBird: ', e)
         }
       }
     }
 
-    async flipSaleStatus(e) {
+
+    async connectWeb3(e) {
       e.preventDefault()
-      if(this.state.token !== 'undefined') {
-        try {
-          await this.state.token.methods.flipSaleState().send({from: this.state.account})
-        } catch (e) {
-          console.log('Error, flipSaleState: ', e)
-        }
+      if(typeof window.ethereum != 'undefined') {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        this.setState({account: account})
+      } else {
+        window.alert("Please install MetaMask")
+      }
+    }
+
+    async showConnectWeb3Button() {
+      if(window.ethereum) {
+        await window.ethereum.send('eth_requestAccounts')
+        return true
+      } else {
+        return false
       }
     }
 
     constructor(props) {
       super(props)
       this.state = {
-        web: 'undefined',
+        web3: 'undefined',
         account: '',
         token: null,
-        balance: 0
+        balance: 0,
       }
     }
 
     render() {
       return (
-        <div className="text-monospace">
-          <button type='submit' className='btn btn-primary' onClick={(e) => this.mintBird(e)}>MINT</button>
-          <button type='submit' className='btn btn-primary' onClick={(e) => this.flipSaleStatus(e)}>FlipSaleStatus</button>
-        </div>
-      )
-    }
+
+        <main role="main" className="col-lg-12 d-flex justify-content-center bg-dark text-white">
+          <div className="content mr-auto ml-auto">
+              <div>
+                  <br></br>
+                  How many birds to mint?
+                  <br></br>
+                  (each bird costs 0.02 ETH + gas fees)
+                  <br></br>
+                  (can mint upto 69 birds per account)
+                  <br></br>
+                  <form onSubmit={(e) => {
+                    e.preventDefault()
+                    let amount = this.amountOfBirds.value
+                    let total = amount * 10**16*2// convert to wei
+                    this.mintBirds(amount, total)
+                  }}>
+                  <div className='form-group mr-sm-2'>
+                  <br></br>
+                    <input
+                      id='numBirds'
+                      step='1'
+                      type='number'
+                      className="form-control form-control-md"
+                      placeholder="amount..."
+                      ref={(input) => { this.amountOfBirds = input }}
+                    />
+                  </div>
+                  <button type='submit' className='btn btn-primary'>MINT</button>
+                </form>
+              </div>
+            <div>
+              { this.showConnectWeb3Button() ? "Web3 is Connected!" : <button type='submit' className='btn btn-primary' onClick={(e) => this.connectWeb3(e)}>Connect Web3</button> }
+          </div>
+          </div>
+        </main>
+    );
+  }
 
 }
 
