e2065313cd690b4cf2ad09d94259ddbd88d0dc77
ask03 <aykim043@gmail.com>
2021-09-10 08:47:40 +0900

    polish before mainnet

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,6 +3,5 @@ const PlayBirdMansion = artifacts.require("PlayBirdMansion")
 module.exports = async function(deployer) {
   const name = "Play Bird Mansion"
   const symbol = "PBM"
-  const MAX_BIRDS = 6969
-  await deployer.deploy(PlayBirdMansion, name, symbol, MAX_BIRDS)
+  await deployer.deploy(PlayBirdMansion, name, symbol)
 }
--- src/contracts/PlayBirdMansion.sol
+++ src/contracts/PlayBirdMansion.sol
@@ -4,22 +4,24 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PlayBirdMansion is ERC721Enumerable, Ownable {
-  uint public constant maxBirdPurchase = 69;
-  uint256 public MAX_BIRDS;
-  uint256 public birdPrice = 20000000000000000; // 0.02 ETH
+  uint256 public constant maxBirdPurchase = 69;
+
+  uint256 public constant birdPrice = 20000000000000000; // 0.02 ETH
+
+  uint256 public constant MAX_BIRDS = 6969;
+
   bool public saleActive = false;
+
   string public baseURI;
 
-  constructor (string memory name, string memory symbol, uint256 maxSupply) ERC721(name, symbol) {
-      MAX_BIRDS = maxSupply;
-  }
+  constructor (string memory name, string memory symbol, uint256 maxSupply) ERC721(name, symbol) {}
 
   function withdraw() public onlyOwner {
       uint balance = address(this).balance;
       payable(msg.sender).transfer(balance);
   }
 
-  function setURI(string newURI) public onlyOwner {
+  function setBaseURI(string memory newURI) public onlyOwner {
       baseURI = newURI;
   }
 
@@ -27,10 +29,14 @@ contract PlayBirdMansion is ERC721Enumerable, Ownable {
       return baseURI;
   }
 
-  function reserveBirds() public onlyOwner {
-      uint supply = totalSupply();
-      for (uint i = 0; i < 100; i++) {
-          _safeMint(msg.sender, supply + 1);
+  function reserveBirds(uint256 amount) public onlyOwner {
+      require(totalSupply() + amount <= MAX_BIRDS, "Reservation would exceed max amount of birds");
+
+      for(uint i = 0; i < amount; i++) {
+          uint index = totalSupply() + 1;
+          if(totalSupply() < MAX_BIRDS) {
+            _safeMint(msg.sender, index);
+          }
       }
   }
 
@@ -40,15 +46,15 @@ contract PlayBirdMansion is ERC721Enumerable, Ownable {
 
   function mintBird(uint numberOfBirds) public payable {
       require(saleActive, "Sale is not active");
-      require(numberOfBirds <= maxBirdPurchase, "Can only mint 30 birds at a time");
+      require(numberOfBirds <= maxBirdPurchase, "Can only mint 69 birds at a time");
       require(totalSupply() + numberOfBirds <= MAX_BIRDS, "Purchase would exceed max supply of Birds");
       require(birdPrice * numberOfBirds <= msg.value, "Ether value sent is incorrect");
 
       for(uint i = 0; i < numberOfBirds; i++) {
-        uint index = totalSupply() + 1;
-        if (totalSupply() < MAX_BIRDS) {
-            _safeMint(msg.sender, index);
-        }
+          uint index = totalSupply() + 1;
+          if (totalSupply() < MAX_BIRDS) {
+              _safeMint(msg.sender, index);
+          }
       }
   }
 
