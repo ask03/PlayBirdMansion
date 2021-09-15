032877a3b0b89566b8e673389db52e48abf3eff5
ask03 <aykim043@gmail.com>
2021-09-15 09:13:23 +0900

    app updated

--- src/components/App.js
+++ src/components/App.js
@@ -119,6 +119,7 @@ class App extends Component {
 
     render() {
       return (
+      
 
         <main role="main" className="col-lg-12 d-flex justify-content-center text-white ">
           <div className="content mr-auto ml-auto">
